3bb906b21e1fa815f13856f658a0477413a9316e
“The <chatethakhun.jack@gmail.com>
2024-01-10 12:02:59 +0700

    add login and signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name avatar])
+  end
   def hello
     render json: {
       message: 'Hello, World!'
