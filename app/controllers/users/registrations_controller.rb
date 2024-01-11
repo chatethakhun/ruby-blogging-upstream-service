e450b4cf5c3a94a1d80fca006d5f3a88df11098b
“The <chatethakhun.jack@gmail.com>
2024-01-11 19:52:38 +0700

    return token in register

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,7 +10,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
     if resource.persisted?
       render json: {
         status: {code: 200, message: 'Signed up successfully.'},
-        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes],
+        token: JWT.encode({user_id: current_user.id}, Rails.application.credentials.devise_jwt_secret_key!)
       }
     else
       render json: {
