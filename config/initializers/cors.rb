333a4535f0c7a9945eb1c8457410671f2e282c1f
“The <chatethakhun.jack@gmail.com>
2024-01-12 20:47:23 +0700

    expose header

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,9 +8,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
-
     resource "*",
-      headers: :any,
+      headers: %w(Authorization),
+      expose: %w(Authorization),
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
