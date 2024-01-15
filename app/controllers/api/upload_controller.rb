985a20af55e6c115ef1b83d553112874c1f5e16d
“The <chatethakhun.jack@gmail.com>
2024-01-15 19:17:20 +0700

    change cloud storage to cloudary

--- app/controllers/api/upload_controller.rb
+++ app/controllers/api/upload_controller.rb
@@ -1,21 +1,15 @@
 class Api::UploadController < ApplicationController
   before_action :authenticate_user!
   def create
-    response = HTTParty.post("https://file.io",
-      body: { file: params[:file] },
-      headers: { 'Content-Type' => 'multipart/form-data', 'Authorization' => "Bearer #{Rails.application.credentials.file_io_api_key}" })
-
     begin
-      result = JSON.parse(response.body)
-      if result["success"]
-        render json: {
-          link: result["link"]
-        }
-      else
-        raise "Upload failed: #{result['message']}"
-      end
+    response = Cloudinary::Uploader.upload(params[:file],
+      :public_id => (0...8).map { (65 + rand(26)).chr }.join)
+
+    render json: {
+      image_url: response["url"]
+    }
     rescue
-      raise "Invalid response received from the server."
+      raise "Error uploading image"
     end
   end
 end
