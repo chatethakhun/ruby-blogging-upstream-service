1c644fbc420ff3a5da4d04d9b902cdafb6bf8137
TheOmelet <chatethakhun.jack@gmail.com>
2024-01-16 17:22:11 +0700

    imp api update user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,11 +2,23 @@ class Api::UsersController < ApplicationController
   respond_to :json
   before_action :authenticate_user!
   def upload_attachment
-    byebug
     render json: { message: 'required thumbnail url' }, status: :unprocessable_entity unless params[:thumbnail_url].present?
     current_user.thumbnail = params[:thumbnail_url]
     current_user.save!
 
     render json: { message: 'Thumbnail attached successfully' }, status: :ok
+  end
+
+
+  def update
+    current_user.update!(user_params)
+
+    @user = current_user
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :thumbnail)
   end
 end
